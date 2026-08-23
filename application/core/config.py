e7e7c93882bf544fe64de5767f1fab97a832c6bf
Aleksander Kuznetsov <kosogor2709@mail.ru>
2026-08-23 19:43:10 +0300

    db config

--- application/core/config.py
+++ application/core/config.py
@@ -1,16 +1,28 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
+
 
 class RunAppConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 3535
 
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False,
+    echo_pool: bool = False,
+    pool_size: int = 10,
+    max_overflow: int = 10
 
 
 class Settings(BaseSettings):
     run: RunAppConfig = RunAppConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+
 
 settings = Settings()
