374aa536aaeb0044f0986838c3e56f412d175e3a
Aleksander Kuznetsov <kosogor2709@mail.ru>
2026-08-24 21:22:24 +0300

    create model user

--- application/alembic/env.py
+++ application/alembic/env.py
@@ -7,8 +7,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from models import user
 from core.config import settings
 from models.base import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
